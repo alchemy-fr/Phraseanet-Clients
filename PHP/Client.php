ba82f9895b7ee8b3b84644eb3a86fdc0792f6e41
Romain Neutron <imprec@gmail.com>
2011-11-11 01:17:49 +0100

    Fix license and authors

--- PHP/Client.php
+++ PHP/Client.php
@@ -1,15 +1,30 @@
 <?php
 
 /**
- * 
+ * Phraseanet Client Librarie
+ * Copyright (C) 2004 Alchemy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  * PhaseanetClientApi - Provide easy easy access to Phraseanet API
  * 
  * @author https://github.com/alchemy-fr
  * @copyright (c) 2004 Alchemy
  * @licence Licensed under GPL license.
  * @see http://phraseanet.com/license
+ * @see http://developer.phraseanet.com
  * @version 1.0
  * @link https://github.com/alchemy-fr/PhraseanetApiClient
+ * 
  */
 require_once dirname(__FILE__) . '/CurlWrapper/CurlWrapper.php';
 
