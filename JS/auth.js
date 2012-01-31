697bc5bf216df392c12dc60c08fa31776e26fddc
Ysolyne Gresille <ygresille@alchemy.fr>
2012-01-31 11:05:32 +0100

    Objects Auth & QF modified

---
+++ JS/auth.js
@@ -0,0 +1,99 @@
+/**
+ * Phraseanet Client Librarie
+ * Copyright (C) 2004 Alchemy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PhraseanetClientApi - Provide easy easy access to Phraseanet API
+ * 
+ * @author https://github.com/alchemy-fr
+ * @copyright (c) 2004 Alchemy
+ * @licence Licensed under GPL license.
+ * @see http://phraseanet.com/license
+ * @see http://developer.phraseanet.com
+ * @version 1.0
+ * @link https://github.com/alchemy-fr/Phraseanet-Clients
+ * 
+**/
+
+/**
+ * Phraseanet - Gestion du processus d'authentification
+ */
+
+(function(window)
+{
+
+    /**
+	 * Constructeur de l'objet Auth
+	 * 
+	 * @param phraseanet {Phraseanet} objet Phraseanet lié à l'objet Auth
+	 */
+    var Auth = function(phraseanet)
+    {
+        this._phraseanet = phraseanet;
+    };
+	
+    Auth.prototype =
+    {
+		
+			
+        /** Construit l'url de réponse d'authentification */
+        buildAuthUrl: function (options)
+        {
+            var options = options || {};
+            var auth_params =
+            {
+                redirect_uri: window.location.href,
+                response_type: 'token',
+                client_id: phraseanet._apiKey
+            }
+				
+            return phraseanet._domain.authorize + '?' + phraseanet._query_formater.encode(jQuery.extend(auth_params, options), '&', true);
+        },
+
+        /** Décode l'url de réponse d'authentification */			
+        readFragment: function()
+        {
+            var fragment = window.location.hash.replace('%23', '#').replace('#', '');
+				
+            return phraseanet._query_formater.decode(fragment);
+        },
+
+		/** Initialise la session */
+        initSession: function()
+        {
+            var fragment = this.readFragment();
+				
+            if (fragment)
+            {
+                if ('error' in fragment)
+                    throw ('Received auth error `' + fragment.error + '\': ' + fragment.error_description);					
+                if ('access_token' in fragment)
+                {
+                    var session =
+                    {
+                        access_token: fragment.access_token,
+                        user: phraseanet._apiKey
+                    };
+						
+                    phraseanet.setSession(session);
+                    phraseanet._cookie.set();
+						
+                    return;
+                }
+            }
+        }
+	};
+    
+    window.Auth = Auth;
+
+})(window);
